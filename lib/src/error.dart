fb5530e639c08a0af2e745037d4d2024039b0ea2
jonahwilliams <campfish91@gmail.com>
2016-10-01 10:32:49 -0700

    FriendlyError is now SourceError, and just exposes a span

--- lib/src/error.dart
+++ lib/src/error.dart
@@ -1,49 +1,38 @@
 import 'package:source_span/source_span.dart';
 
 /// A pretty error format for the project.
-abstract class FriendlyError extends Error {
-  /// the line number of the error.
-  String get line;
-
-  /// the sourceFile (if availible) or None.
-  String get sourceFile;
-
-  /// the text span where the error is located.
-  String get sourceLine;
+abstract class SourceError extends Error {
+  /// The source span where the error occurred.
+  SourceSpan get source;
 
   /// A friendly, Actionable error message.
-  String get errorMessage;
+  String get message;
 
   @override
   String toString() {
-    return '\n---- PARSE ERROR ----------- $sourceFile\n' +
+    final line = span.start.line.toString();
+    return '\n---- PARSE ERROR ----------- ${span.sourceUrl?.path ?? 'No File'}\n' +
         '\n' +
         'There was a problem while parsing the following line:\n' +
-        '$line| $sourceLine\n' +
-        '${' ' * (line.length + sourceLine.length + 1)}^\n' +
-        '$errorMessage\n';
+        '$line| ${source.text}\n' +
+        '${' ' * (line.length + source.text.length + 1)}^\n' +
+        '$message\n';
   }
 }
 
 /// Simple errors which can occur during lexical analysis.
-class LexerError extends FriendlyError {
+class LexerError extends SourceError {
   /// The source span where the error was encountered, from column 0.
-  final SourceSpan span;
+  final SourceSpan source;
 
   /// Additional lexer context into the failure.
   final LexerErrorKind kind;
 
   /// Used to signal possible malformed html to the parser.
-  LexerError(this.span, this.kind);
+  LexerError(this.source, this.kind);
 
   @override
-  String get sourceFile => span.sourceUrl?.path ?? 'No File';
-  @override
-  String get sourceLine => span.text;
-  @override
-  String get line => '${span.start.line}';
-  @override
-  String get errorMessage {
+  String get message {
     switch (kind) {
       case LexerErrorKind.misMatchedClose:
         return 'I found an unnecessary closing tag.';
